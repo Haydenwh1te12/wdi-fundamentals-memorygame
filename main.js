e20cdb0ca8c09e91e5847e4cc746ff9a1ac494e8
Haydenwh1te12 <white1hcw@gmail.com>
2017-05-10 21:10:24 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
- console.log("Up and running!");
- var cardOne = "king";
- var cardTwo = "king";
- var cardThree = "queen";
- var cardFour = "queen";
-   console.log("User flipped" + cardOne);
-     console.log(" User flipped " + cardTwo);
+
+var cards = ["queen" , "queen" , "king" , "king"];
+
+var cardsInPlay = [];
+
+function checkForMatch() {
+	
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	}  else {
+	  console.log("Sorry, try again.");
+	}
+};
+
+
+var flipCard = function(cardId) {
+    console.log("User flipped " + cards[cardId]);
+	//console.log("cards " + [cardId]);
+	// console.log("Up and running!");
+	cardsInPlay.push("cards " + [cardId]);
+    checkForMatch();
+};
+
+
+ flipCard(0);
+ flipCard(2);
